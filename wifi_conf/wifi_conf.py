e320140e2f8b63f3cc4531fccb8ca23cab2788bd
Moritz Bunse <mbunse@hotmail.com>
2018-03-13 23:03:36 +0000

    Initial import

---
+++ wifi_conf/wifi_conf.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import re
+
+class Wifi_Conf:
+    def __init__(self, config_file):
+        self.__config_file = config_file
+    def set_wifi_ssid_and_password(self, ssid, password):
+        ret = subprocess.check_output(['wpa_passphrase',ssid ,password])
+        with open(self.__config_file, "r+") as f:
+            conf = "".join(f.readlines())
+            new_conf = re.sub('network={.*?}', ret, conf, flags=re.DOTALL)
+        return
+    def reconfigure(self):
+        subprocess.call(['wpa_cli', '-i', 'wlan0', 'reconfigure'])
+        return
+
+
